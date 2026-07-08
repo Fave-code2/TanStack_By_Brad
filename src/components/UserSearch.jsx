aecc6b4c4115f90c43795570f7361e4e2f4d63b6
Uchechukwu-best <uchman6122@gmail.com>
2026-07-08 16:05:20 +0100

    code clean up

--- src/components/UserSearch.jsx
+++ src/components/UserSearch.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { FaGithubAlt } from "react-icons/fa";
+import { fetchGithubUser } from "../api/github";
+import UserCard from "./UserCard";
 
 const UserSearch = () => {
   const [username, setUserame] = useState("");
@@ -8,17 +9,7 @@ const UserSearch = () => {
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["users", submittedUsername],
-    queryFn: async () => {
-      const res = await fetch(
-        `${import.meta.env.VITE_GITHUB_API_URL}/users/${submittedUsername}`,
-      );
-
-      if (!res.ok) throw new Error("User not found");
-
-      const data = await res.json();
-      console.log(data);
-      return data;
-    },
+    queryFn: () => fetchGithubUser(submittedUsername),
 
     // This !! turns it to a boolean while enabled makes it run if it true
     enabled: !!submittedUsername,
@@ -45,21 +36,7 @@ const UserSearch = () => {
       {isLoading && <p className="status">Loading...</p>}
       {isError && <p className="status error">{error.message}</p>}
 
-      {data && (
-        <div className="user-card">
-          <img src={data.avatar_url} alt={data.name} className="avatar" />
-          <h2>{data.name || data.login}</h2>
-          <p className="bio">{data.bio}</p>
-          <a
-            href={data.html_url}
-            className="profile-btn"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGithubAlt /> View Github Profile
-          </a>
-        </div>
-      )}
+      {data && <UserCard user={data} />}
     </>
   );
 };
