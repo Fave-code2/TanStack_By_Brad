a66b607d12bd7d80c162a4e17a2c7bc1a6954251
Uchechukwu-best <uchman6122@gmail.com>
2026-07-08 19:56:08 +0100

    Created suggestion dropdown component

---
+++ src/components/SuggestionDropdown.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const SuggestionDropdown = ({ show, suggestions, onSelect }) => {
+  if (!show || suggestions.length === 0) return null;
+
+  return (
+    <>
+      <ul className="suggestions">
+        {suggestions.slice(0, 5).map((user) => (
+          <li
+            key={user.login}
+            onClick={() => {
+              onSelect(user.login);
+            }}
+          >
+            <img src={user.avatar_url} alt={user.login} className="avatar-xs" />
+            {user.login}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default SuggestionDropdown;
