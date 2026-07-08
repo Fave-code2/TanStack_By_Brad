f51112319277d1ff13202102a968dca58ba0640f
Uchechukwu-best <uchman6122@gmail.com>
2026-07-08 17:04:11 +0100

    Recent searches features

---
+++ src/components/RecentSearches.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FaClock, FaUser } from "react-icons/fa";
+
+const RecentSearches = ({ users, onSelect }) => {
+  return (
+    <div className="recent-searches">
+      <div className="recent-header">
+        <FaClock />
+        <h3>Recent Searches</h3>
+      </div>
+      <ul>
+        {users.map((user) => (
+          <li key={user}>
+            <button onClick={() => onSelect(user)}>
+              <FaUser className="user-icon" />
+              {user}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecentSearches;
--- src/components/UserSearch.jsx
+++ src/components/UserSearch.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchGithubUser } from "../api/github";
 import UserCard from "./UserCard";
+import RecentSearches from "./RecentSearches";
 
 const UserSearch = () => {
   const [username, setUserame] = useState("");
-  const [submittedUsername, SetSubmittedUserame] = useState("");
+  const [submittedUsername, setSubmittedUserame] = useState("");
+  const [recentUsers, setRecentUsers] = useState([]);
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["users", submittedUsername],
@@ -17,7 +19,14 @@ const UserSearch = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    SetSubmittedUserame(username.trim());
+    const trimed = username.trim();
+    if (!trimed) return;
+    setSubmittedUserame(username.trim());
+
+    setRecentUsers((prev) => {
+      const updated = [trimed, ...prev.filter((u) => u !== trimed)];
+      return updated.slice(0, 5);
+    });
   };
 
   return (
@@ -37,6 +46,16 @@ const UserSearch = () => {
       {isError && <p className="status error">{error.message}</p>}
 
       {data && <UserCard user={data} />}
+
+      {recentUsers.length > 0 && (
+        <RecentSearches
+          users={recentUsers}
+          onSelect={(username) => {
+            setSubmittedUserame(username);
+            setUserame(username);
+          }}
+        />
+      )}
     </>
   );
 };
